d153b3f35fd5a2aee23278d04c745bb9feac09c2
edwinauliano <edwinauliano@gmail.com>
2022-02-07 10:23:35 +0700

    [PWA] -Service workers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'jokes';
+
+  update: boolean = false;
+  joke: any;
+
+  constructor(updates: SwUpdate, private dataService: DataService) {
+    updates.available.subscribe(eve => {
+      // this.update = true;
+      updates.activateUpdate().then(() => document.location.reload());
+    })
+  }
+
+  ngOnInit() {
+    this.dataService.gimmeJokes().subscribe(res => {
+      this.joke = res;
+      console.log(this.joke)
+    })
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataService } from './data.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
